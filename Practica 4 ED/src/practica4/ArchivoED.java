53cbe95d7eada9f80117548943b02cac41f06003
Felix del Rio <35599432+felisito16@users.noreply.github.com>
2018-02-27 12:02:40 +0100

    Update ArchivoED.java
    Repositorio online a local.

--- Practica 4 ED/src/practica4/ArchivoED.java
+++ Practica 4 ED/src/practica4/ArchivoED.java
@@ -4,8 +4,8 @@ public class ArchivoED {
 
 	public static void main(String[] args)
 	{
-		//Hola
-		System.out.println("Número aleatorio: " + Math.random());
-		System.out.println("Otro número aleatorio: " + Math.random());
+		//Traido del repositorio online
+		System.out.println("NÃºmero aleatorio: " + Math.random());
+		System.out.println("Otro nÃºmero aleatorio: " + Math.random());
 	}
 }
