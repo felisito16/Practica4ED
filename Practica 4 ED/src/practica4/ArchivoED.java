5129915303c1dea161c4f8349d4f5ef5285f53e5
Felix del Rio <35599432+felisito16@users.noreply.github.com>
2018-03-08 19:35:54 +0100

    Update ArchivoED.java

--- Practica 4 ED/src/practica4/ArchivoED.java
+++ Practica 4 ED/src/practica4/ArchivoED.java
@@ -4,6 +4,7 @@ public class ArchivoED {
 
 	public static void main(String[] args)
 	{
+		//ME VOY A BAJAR EL CODIGO
 		//Traido del repositorio online
 		System.out.println("Número aleatorio: " + Math.random());
 		System.out.println("Otro número aleatorio: " + Math.random());
