150676441019440c7a8d7e99f0a6e9d4d0234916
Familia del Rio <Familia del Rio@DESKTOP-BPJF01M>
2018-03-08 19:46:24 +0100

    commit double

--- HolaMundo/src/es/studium/HolaMundo/HolaMundo.java
+++ HolaMundo/src/es/studium/HolaMundo/HolaMundo.java
@@ -8,5 +8,10 @@ public class HolaMundo
 	System.out.println("Hola mundo 2!");
 	System.out.println("Hola mundo 3!");
 	System.out.println("Hola mundo 4!");
+	
+	System.out.println("Hola mundo!");
+	System.out.println("Hola mundo 2!");
+	System.out.println("Hola mundo 3!");
+	System.out.println("Hola mundo 4!");
 	}
 }
